fc87083686f38fb6f7d774f31c7e8276bbed7999
zxy <zyinngoo@outlook.com>
2022-09-14 20:05:09 +0800

    lodash.chunk

--- lodash/zyinngoo-lodash.js
+++ lodash/zyinngoo-lodash.js
@@ -1,38 +1,30 @@
 var zyinngoo = {
-  chunk: function (ary, size) {
-    var re = []
-    var arr = []
-    for (var i = 0; i < ary.length; i++) {
-      if (i % size == 0) {
-        re.push(arr)
-        var arr = []
+  chunk:
+    function(ary, size) {
+      var re = []
+      var arr = []
+      for (var i = 0; i < ary.length; i++) {
+        if (i % size == 0) {
+          re.push(arr)
+          var arr = []
+        }
+        arr.push(ary[i])
       }
-      arr.push(ary[i])
+      re.push(arr)
+      re.shift()
+      return re
     }
-    re.push(arr)
-    re.shift()
-    return re
-  }
 
+  compact:
+    function (ary) {
+      var re = []
+      for (var i = 0; i < ary.length; i++) {
+        if (ary[i] == Number(ary[i]) || ary[i] !== 0) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          re.push(ary[i])
+        }
+      }
+      return re
+    }
 
 }
